989e8c0fa5d99a45c1e7b9b111409b5a2c64bc55
ozancelikel <ozansmailcelikel@gmail.com>
2025-04-01 16:08:03 +0200

    barani units of measurement fixes

--- app/barani/service.py
+++ app/barani/service.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from sqlalchemy.ext.asyncio import AsyncSession
 
 import app.crud.barani_sensors as db_service
@@ -7,6 +9,8 @@ from app.logs.config_server_logs import server_logger
 
 async def process_helix_message(db: AsyncSession, message: HelixMessage):
     server_logger.info(f"HELIX -- Received message: {message}")
+
+    message = normalize_helix_message(message)
 
     resp: BaraniHelixSensors = await db_service.create_barani_helix_reading(db, message)
 
@@ -39,3 +43,32 @@ async def process_get_sensor_by_serial_number(db: AsyncSession, sn: str):
     server_logger.info(response)
 
     return response
+
+def normalize_helix_message(message: HelixMessage)->HelixMessage:
+    copied = message.model_copy()
+
+    if  copied.temperature and 173.15 < copied.temperature < 373.15:
+        copied.temperature = kelvin_to_celsius(message.temperature)
+        server_logger.info(f"HELIX -- Received message: Message.temperature modified to fix Kelvin values: "
+                      f"{message.temperature} -> {copied.temperature}")
+
+    if  copied.temperature_wetbulb and 173.15 < copied.temperature_wetbulb < 373.15:
+        copied.temperature_wetbulb = kelvin_to_celsius(message.temperature_wetbulb)
+        server_logger.info(f"HELIX -- Received message: Message.temperature_wetbulb modified to fix Kelvin values: "
+                      f"{message.temperature_wetbulb} -> {copied.temperature_wetbulb}")
+
+    if  copied.dew_point and 173.15 < copied.dew_point < 373.15:
+        copied.dew_point = kelvin_to_celsius(message.dew_point)
+        server_logger.info(f"HELIX -- Received message: Message.dew_point modified to fix Kelvin values: "
+                      f"{message.dew_point} -> {copied.dew_point}")
+
+    return copied
+
+def kelvin_to_celsius(kelvin: Optional[float])->Optional[float]:
+    """
+    Converts Kelvin to Celsius.
+
+    :param kelvin:
+    :return:
+    """
+    return round(kelvin - 273.15, 2) if kelvin is not None else None
